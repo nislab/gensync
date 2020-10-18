7597825f21326818751b3410a6d34554aff6ba78
Novak Boškov <boskov@bu.edu>
2020-10-18 18:04:53 -0400

    Indentation

--- src/Benchmarks/Runner.cpp
+++ src/Benchmarks/Runner.cpp
@@ -79,84 +79,84 @@ inline void addAnElemWithReps(shared_ptr<GenSync> gs,
                               shared_ptr<DataObject> obj);
 
 int main(int argc, char *argv[]) {
-  BenchParams bPar = BenchParams{argv[1]};
-  GenSyncPair genSyncs = buildGenSyncs(bPar, argc >= 3);
+    BenchParams bPar = BenchParams{argv[1]};
+    GenSyncPair genSyncs = buildGenSyncs(bPar, argc >= 3);
 
-  Logger::gLog(Logger::TEST, "Sets are ready, reconciliation starts...");
+    Logger::gLog(Logger::TEST, "Sets are ready, reconciliation starts...");
 
-  pid_t pid = fork();
-  if (pid == 0) { // child process
-    try {
-      genSyncs.first->serverSyncBegin(0);
-    } catch (exception &e) {
-      cout << "Sync Exception [server]: " << e.what() << "\n";
+    pid_t pid = fork();
+    if (pid == 0) { // child process
+        try {
+            genSyncs.first->serverSyncBegin(0);
+        } catch (exception &e) {
+            cout << "Sync Exception [server]: " << e.what() << "\n";
+        }
+    } else if (pid > 0) { // parent process
+        try {
+            genSyncs.second->clientSyncBegin(0);
+        } catch (exception &e) {
+            cout << "Sync Exception [client]: " << e.what() << "\n";
+        }
+    } else if (pid < 0) {
+        throw runtime_error("Fork has failed");
     }
-  } else if (pid > 0) { // parent process
-    try {
-      genSyncs.second->clientSyncBegin(0);
-    } catch (exception &e) {
-      cout << "Sync Exception [client]: " << e.what() << "\n";
-    }
-  } else if (pid < 0) {
-    throw runtime_error("Fork has failed");
-  }
 }
 
 GenSyncPair buildGenSyncs(const BenchParams &par, bool zipfElems) {
-  GenSync::Builder serverBuilder = GenSync::Builder()
-                                       .setComm(GenSync::SyncComm::socket)
-                                       .setProtocol(par.syncProtocol);
-  GenSync::Builder clientBuilder = GenSync::Builder()
-                                       .setComm(GenSync::SyncComm::socket)
-                                       .setProtocol(par.syncProtocol);
+    GenSync::Builder serverBuilder = GenSync::Builder()
+                                         .setComm(GenSync::SyncComm::socket)
+                                         .setProtocol(par.syncProtocol);
+    GenSync::Builder clientBuilder = GenSync::Builder()
+                                         .setComm(GenSync::SyncComm::socket)
+                                         .setProtocol(par.syncProtocol);
 
-  par.syncParams->apply(serverBuilder);
-  par.syncParams->apply(clientBuilder);
-  auto server = make_shared<GenSync>(serverBuilder.build());
-  auto client = make_shared<GenSync>(clientBuilder.build());
+    par.syncParams->apply(serverBuilder);
+    par.syncParams->apply(clientBuilder);
+    auto server = make_shared<GenSync>(serverBuilder.build());
+    auto client = make_shared<GenSync>(clientBuilder.build());
 
-  if (zipfElems) {
-    size_t cardA = DEFAULT_CARD;
-    size_t cardB = DEFAULT_CARD;
-    size_t common = comm(gen);
+    if (zipfElems) {
+        size_t cardA = DEFAULT_CARD;
+        size_t cardB = DEFAULT_CARD;
+        size_t common = comm(gen);
 
-    do {
-      cardA = card(gen);
-    } while (cardA < common + 1); // at least one local element
+        do {
+            cardA = card(gen);
+        } while (cardA < common + 1); // at least one local element
 
-    do {
-      cardB = card(gen);
-    } while (cardB < common + 1);
+        do {
+            cardB = card(gen);
+        } while (cardB < common + 1);
 
-    stringstream ss;
-    ss << "Benchmarks generated sets:  Client: " << cardA
-       << ", Server: " << cardB << ", Common: " << common;
-    Logger::gLog(Logger::TEST, ss.str());
+        stringstream ss;
+        ss << "Benchmarks generated sets:  Client: " << cardA
+           << ", Server: " << cardB << ", Common: " << common;
+        Logger::gLog(Logger::TEST, ss.str());
 
-    Logger::gLog(Logger::TEST, "Adding common elements...");
-    for (size_t ii = 0; ii < common; ii++) {
-      auto obj = makeObj();
-      addAnElemWithReps(server, obj);
-      addAnElemWithReps(client, obj);
+        Logger::gLog(Logger::TEST, "Adding common elements...");
+        for (size_t ii = 0; ii < common; ii++) {
+            auto obj = makeObj();
+            addAnElemWithReps(server, obj);
+            addAnElemWithReps(client, obj);
+        }
+
+        Logger::gLog(Logger::TEST, "Adding server local elements...");
+        for (size_t ii = 0; ii < (cardA - common); ii++)
+            addAnElemWithReps(server, makeObj());
+
+        Logger::gLog(Logger::TEST, "Adding client local elements...");
+        for (size_t ii = 0; ii < (cardB - common); ii++)
+            addAnElemWithReps(client, makeObj());
+
+    } else {
+        for (auto elem : *par.serverElems)
+            server->addElem(make_shared<DataObject>(elem));
+
+        for (auto elem : *par.clientElems)
+            client->addElem(make_shared<DataObject>(elem));
     }
 
-    Logger::gLog(Logger::TEST, "Adding server local elements...");
-    for (size_t ii = 0; ii < (cardA - common); ii++)
-      addAnElemWithReps(server, makeObj());
-
-    Logger::gLog(Logger::TEST, "Adding client local elements...");
-    for (size_t ii = 0; ii < (cardB - common); ii++)
-      addAnElemWithReps(client, makeObj());
-
-  } else {
-    for (auto elem : *par.serverElems)
-      server->addElem(make_shared<DataObject>(elem));
-
-    for (auto elem : *par.clientElems)
-      client->addElem(make_shared<DataObject>(elem));
-  }
-
-  return {server, client};
+    return {server, client};
 }
 
 /**
@@ -166,53 +166,53 @@ GenSyncPair buildGenSyncs(const BenchParams &par, bool zipfElems) {
  * TODO: This should be implemented as a part of RandGen
  */
 int zipf(int n, double alpha) {
-  // The map of c constants for Zipfian distributions.
-  // The idea is that all the calls with the same n sample from the exact same
-  // distribution. Thus for each n, c need be calculated only once.
-  static map<int, double> c_map;
+    // The map of c constants for Zipfian distributions.
+    // The idea is that all the calls with the same n sample from the exact same
+    // distribution. Thus for each n, c need be calculated only once.
+    static map<int, double> c_map;
 
-  double c = 0;          // Normalization constant
-  double z = 0;          // Uniform random number (0 < z < 1)
-  double sum_prob = 0;   // Sum of probabilities
-  double zipf_value = 0; // Computed exponential value to be returned
-  int i = 0;             // Loop counter
+    double c = 0;          // Normalization constant
+    double z = 0;          // Uniform random number (0 < z < 1)
+    double sum_prob = 0;   // Sum of probabilities
+    double zipf_value = 0; // Computed exponential value to be returned
+    int i = 0;             // Loop counter
 
-  if (c_map.find(n) == c_map.end()) {
-    for (i = 1; i <= n; i++)
-      c = c + (1.0 / pow((double)i, alpha));
+    if (c_map.find(n) == c_map.end()) {
+        for (i = 1; i <= n; i++)
+            c = c + (1.0 / pow((double)i, alpha));
 
-    c = 1.0 / c;
-    c_map.insert(std::pair<int, double>(n, c));
-  } else {
-    c = c_map.at(n);
-  }
-
-  // Pull a uniform random number (0 < z < 1)
-  do {
-    z = dist(gen);
-  } while (z == 0 || z == 1);
-
-  // Map z to the value
-  sum_prob = 0;
-  for (i = 1; i <= n; i++) {
-    sum_prob = sum_prob + c / pow((double)i, alpha);
-    if (sum_prob >= z) {
-      zipf_value = i;
-      break;
+        c = 1.0 / c;
+        c_map.insert(std::pair<int, double>(n, c));
+    } else {
+        c = c_map.at(n);
     }
-  }
 
-  // Assert that zipf_value is between 1 and N
-  assert((zipf_value >= 1) && (zipf_value <= n));
+    // Pull a uniform random number (0 < z < 1)
+    do {
+        z = dist(gen);
+    } while (z == 0 || z == 1);
 
-  return zipf_value;
+    // Map z to the value
+    sum_prob = 0;
+    for (i = 1; i <= n; i++) {
+        sum_prob = sum_prob + c / pow((double)i, alpha);
+        if (sum_prob >= z) {
+            zipf_value = i;
+            break;
+        }
+    }
+
+    // Assert that zipf_value is between 1 and N
+    assert((zipf_value >= 1) && (zipf_value <= n));
+
+    return zipf_value;
 }
 
 shared_ptr<DataObject> makeObj() {
-  return make_shared<DataObject>(DataObject(ZZ(elem(rd))));
+    return make_shared<DataObject>(DataObject(ZZ(elem(rd))));
 }
 
 void addAnElemWithReps(shared_ptr<GenSync> gs, shared_ptr<DataObject> obj) {
-  for (size_t rep = 0; rep < zipf(MAX_CARD / REP_RATIO); rep++)
-    gs->addElem(obj);
+    for (size_t rep = 0; rep < zipf(MAX_CARD / REP_RATIO); rep++)
+        gs->addElem(obj);
 }
