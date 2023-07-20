250a1bb3842b32c77535ceb5c14550bbafeb105c
Xingyu <chxy517@bu.edu>
2023-07-20 15:16:50 -0400

    clean up

--- src/Aux/UID.cpp
+++
@@ -1,4 +0,0 @@
-/* This code is part of the GenSync project developed at Boston University.  Please see the README for use and references. */
-
-#include <GenSync/Aux/UID.h>
-int UID::ID_count = 0; // initialize the static field
